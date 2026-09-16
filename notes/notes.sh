0d3528ee9ab19015cd7a34cf3cf756609e7fd3f7
Natalia Lopez Anguita <b12loann@gmail.com>
2026-09-16 09:54:51 +0000

    add homework and notes

---
+++ notes/notes.sh
@@ -0,0 +1,6 @@
+# Create a conda environment
+conda create -n ml-zoomcamp python=3.11
+## to activate environment
+conda activate ml-zoomcamp
+## to install packages
+conda install numpy pandas scikit-learn seaborn jupyter
