480fa2c81408a04abfdcc0c6bc580f7c001ea532
Natalia Lopez Anguita <b12loann@gmail.com>
2026-09-16 10:02:00 +0000

    notes

--- notes/notes.sh
+++ notes/notes.sh
@@ -4,3 +4,19 @@ conda create -n ml-zoomcamp python=3.11
 conda activate ml-zoomcamp
 ## to install packages
 conda install numpy pandas scikit-learn seaborn jupyter
+conda install -c conda-forge jupyterlab jupyterlab-git
+## open jupyter lab
+jupyter-lab
+
+# Github
+## Check for changes
+git status
+## Apply changes
+git add homework/
+git add notes/
+## Commit changes
+git commit -m "add homework and notes"
+## Push changes to remote repository
+git push
+
+
